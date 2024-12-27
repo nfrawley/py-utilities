62d7ff55a70e2e0bf10af765d0e4441ac78d4d61
Noah Frawley <102563515+nfrawley@users.noreply.github.com>
2024-12-26 22:38:00 -0800

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 import os, dotenv, logging, datetime, platform, subprocess, socket, winwifi
 from pathlib import Path
-
+# TEST
 class env:
     @staticmethod
     def load(file: str, setting: str) -> dict:
